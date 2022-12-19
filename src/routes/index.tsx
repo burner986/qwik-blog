961d8fe497c4be6e251408907f4ec736a59b8ff0
Bernd Dabrock <bernd.dabrock@gmail.com>
2022-12-19 22:50:03 +0800

    wip

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,13 @@ export default component$(() => {
   return (
     <>
       <div
-        class="hero h-[70vh]"
+        class="hero h-[65vh]"
         style="background-image: url(https://placeimg.com/1000/800/arch);"
       >
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
           <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 class="mb-5 text-7xl font-bold">Hello there</h1>
             <p class="mb-5">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
@@ -22,24 +22,38 @@ export default component$(() => {
           </div>
         </div>
       </div>
-      <div class="flex flex-col w-full border-opacity-50 pt-16 pb-16 pr-80 pl-80">
-        <div class="card w-auto bg-base-100 shadow-xl">
-          <div class="card-body">
-            <h2 class="card-title">Card title!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <button class="btn btn-primary">Buy Now</button>
-            </div>
+      <div class="flex justify-center">
+        <div class="flex flex-col w-full max-w-3xl border-opacity-50 p-3 pt-16 pb-16">
+          <div class="">
+            <a href="">
+              <h2 class="post-title text-5xl pb-3 font-bold">
+                Man must explore, and this is exploration at its greatest
+              </h2>
+              <h3 class="post-subtitle text-3xl pb-3 font-thin">
+                Problems look mighty small from 150 miles up
+              </h3>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="#!">Start Bootstrap</a>
+              on September 24, 2022
+            </p>
           </div>
-        </div>
-        <div class="divider"></div>
-        <div class="card w-auto bg-base-100 shadow-xl">
-          <div class="card-body">
-            <h2 class="card-title">Card title!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-              <button class="btn btn-primary">Buy Now</button>
-            </div>
+          <div class="divider"></div>
+          <div class="">
+            <a href="">
+              <h2 class="post-title text-5xl pb-3 font-bold">
+                Man must explore, and this is exploration at its greatest
+              </h2>
+              <h3 class="post-subtitle text-3xl pb-3 font-thin">
+                Problems look mighty small from 150 miles up
+              </h3>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="#!">Start Bootstrap</a>
+              on September 24, 2022
+            </p>
           </div>
         </div>
       </div>
